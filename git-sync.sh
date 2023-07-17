d11b5b1886c490bfe7fd60c43519d0d23b35b1cc
Matteo Rigoni <rigonimat@gmail.com>
2023-07-17 14:43:48 +0200

    feat: depth 1 cloning

--- git-sync.sh
+++ git-sync.sh
@@ -30,7 +30,7 @@ echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
 if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
   # Clone using source ssh key if provided
-  git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
+  git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source --depth 1 && cd /root/source
 else
   git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
 fi
